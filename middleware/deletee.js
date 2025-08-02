3ab5b03ca82fa92a8a030ade64b427700e399561
Amit Kumar Dey <amitdey861744@gmail.com>
2025-08-02 14:58:46 +0530

    finallllll6 commit

--- middleware/deletee.js
+++ middleware/deletee.js
@@ -9,6 +9,15 @@ async function deletee(req, res, next) {
 
     for (const appo of appointments) {
       if (new Date(appo.date) < now) {
+        let a= await appoModel.findOne({_id:appo._id});
+        let u= await userModel.findOne({_id:a.userid});
+        let d= await doctorModel.findOne({_id:a.doctorid});
+        await userModel.findOneAndUpdate({_id:a.userid},{
+            drList: u.drList.filter((dr)=>dr.toString() != d._id.toString())
+        })
+        await doctorModel.findOneAndUpdate({_id:a.doctorid},{
+            userList: d.userList.filter((user)=>user.toString() != u._id.toString())
+        })
         await appoModel.findByIdAndDelete(appo._id);
       }
     }
